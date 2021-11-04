29e925baffc09b44f0532af6f68b66e7e0f6907a
Gabriel Margolis <gmargo@mit.edu>
2021-11-04 18:54:44 -0400

    add mini cheetah, but turning off heightmap seems broken

--- legged_gym/utils/terrain.py
+++ legged_gym/utils/terrain.py
@@ -32,7 +32,7 @@ import numpy as np
 from numpy.random import choice
 from scipy import interpolate
 
-from isaacgym import terrain_utils
+#from isaacgym import terrain_utils
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg
 
 class Terrain:
