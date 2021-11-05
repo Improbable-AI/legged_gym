7219cf11bf9a9175f29bf8dfbb7e34f3e14d14d9
Gabriel Margolis <gmargo@mit.edu>
2021-11-05 11:29:41 -0400

    training!

--- legged_gym/envs/mini_cheetah/mini_cheetah_config.py
+++ legged_gym/envs/mini_cheetah/mini_cheetah_config.py
@@ -3,7 +3,7 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class MiniCheetahCfg( LeggedRobotCfg ):
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        pos = [0.0, 0.0, 0.32] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
             'FL_hip_joint': 0.1,   # [rad]
             'RL_hip_joint': 0.1,   # [rad]
@@ -34,9 +34,11 @@ class MiniCheetahCfg( LeggedRobotCfg ):
     class asset( LeggedRobotCfg.asset ):
         file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/mini_cheetah/urdf/mini_cheetah.urdf'
         foot_name = "calf"
-        penalize_contacts_on = ["thigh", "calf"]
-        terminate_after_contacts_on = ["base"]
-        self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
+        penalize_contacts_on = []
+        terminate_after_contacts_on = ["base", "thigh"]
+        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter
+        flip_visual_attachments = False
+        fix_base_link = False
   
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
@@ -48,6 +50,10 @@ class MiniCheetahCfg( LeggedRobotCfg ):
     class terrain( LeggedRobotCfg.terrain ):
         mesh_type = 'plane'
         measure_heights = False
+
+    class env( LeggedRobotCfg.env ):
+        num_observations = 48
+
 
 class MiniCheetahCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
